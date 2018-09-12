43802e997fad54fcd3764bf45cbe3c5c56b7139c
smeeton1 <smeeton1>
2018-09-12 11:40:46 -0500

    testing looping the function

--- htest.py
+++ htest.py
@@ -9,39 +9,42 @@ import scipy.integrate as integrate
 xrang= numpy.arange(-2,2,0.01)
 yrang= numpy.arange(-2,2,0.01)
 
-VeB=[1,0,0,0,0,0,0,0,0,0,0]
 
-rho=numpy.outer(VeB,VeB)
-nrho=len(rho)
+for p in range(0,5)
+  VeB=[0,0,0,0,0,0,0,0,0,0,0]
+  VeB[p]=1
 
-Qfun=[[0 for i in xrange(len(xrang))] for i in xrange(len(yrang))]
+  rho=numpy.outer(VeB,VeB)
+  nrho=len(rho)
 
-h=0
-g=0
-alpha=numpy.empty([nrho],dtype=complex)
-for i in xrang:
-  for l in yrang:
-    for k in range(0,nrho):
-      alpha[k]=cmath.exp(-pow(abs(i+1j*l),2)/2)*(pow((i+1j*l),k)/math.sqrt(math.factorial(k)))
-      
-    Qfun[g][h]=(numpy.dot(numpy.transpose(alpha),numpy.dot(rho,alpha.conjugate()))).real
-    g=g+1
-    
-  h=h+1
+  Qfun=[[0 for i in xrange(len(xrang))] for i in xrange(len(yrang))]
+
+  h=0
   g=0
-  
+  alpha=numpy.empty([nrho],dtype=complex)
+  for i in xrang:
+    for l in yrang:
+      for k in range(0,nrho):
+	alpha[k]=cmath.exp(-pow(abs(i+1j*l),2)/2)*(pow((i+1j*l),k)/math.sqrt(math.factorial(k)))
+	
+      Qfun[g][h]=(numpy.dot(numpy.transpose(alpha),numpy.dot(rho,alpha.conjugate()))).real
+      g=g+1
+      
+    h=h+1
+    g=0
+    
 
-X, Y = numpy.meshgrid(xrang, yrang)
+  X, Y = numpy.meshgrid(xrang, yrang)
 
-QfunName='Qfun1.eps'
-fig = plt.figure(3)#,figsize=(3,2))
-#ax = fig.add_subplot(111, projection='3d')
-#ax.plot_surface(X,Y,Qfun)
-plt.matshow(Qfun)
-plt.savefig(QfunName)
+  QfunName='Qfun1_%i.eps' % (p)
+  fig = plt.figure(3)#,figsize=(3,2))
+  #ax = fig.add_subplot(111, projection='3d')
+  #ax.plot_surface(X,Y,Qfun)
+  plt.matshow(Qfun)
+  plt.savefig(QfunName)
 
-QfunName3d='Qfun3D1.eps'
-fig = plt.figure(4)#,figsize=(3,2))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X,Y,Qfun)
-plt.savefig(QfunName3d)
+  QfunName3d='Qfun3D1_%i.eps' % (p)
+  fig = plt.figure(4)#,figsize=(3,2))
+  ax = fig.add_subplot(111, projection='3d')
+  ax.plot_surface(X,Y,Qfun)
+  plt.savefig(QfunName3d)
