f4aa742fb427e710cd4ba4293bddf2b3ed93cd7c
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2019-09-17 12:14:24 +0100

    test code for tensors

---
+++ qctest.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include "itensor/all.h"
+using namespace itensor;
+
+
+int main(){
+  
+  
+  auto q1=index(4,"q1");
+  auto w1=index(4,"w1");
+  auto q2=index(4,"q2");
+  auto w2=index(4,"w2");
+  
+  
+  auto D1=ITensor(q1);
+  auto D2=ITensor(q2);
+  
+  auto Cnot=ITensor(q1,w1,q2,w2);
+  
+  D1.set(q1=1,1);
+  D2.set(q2=1,1);
+  
+  Cnot.set(q1=0,w1=0,q2=0,w2=0,1);
+  Cnot.set(q1=0,w1=1,q2=0,w2=1,1);
+  Cnot.set(q1=0,w1=2,q2=0,w2=2,1);
+  Cnot.set(q1=0,w1=3,q2=0,w2=3,1);
+  Cnot.set(q1=1,w1=0,q2=1,w2=1,1);
+  Cnot.set(q1=1,w1=1,q2=1,w2=0,1);
+  Cnot.set(q1=1,w1=2,q2=1,w2=3,1);
+  Cnot.set(q1=1,w1=3,q2=1,w2=2,1);
+  Cnot.set(q1=2,w1=0,q2=2,w2=2,1);
+  Cnot.set(q1=2,w1=1,q2=2,w2=3,1);
+  Cnot.set(q1=2,w1=2,q2=2,w2=0,1);
+  Cnot.set(q1=2,w1=3,q2=2,w2=1,1);
+  Cnot.set(q1=3,w1=0,q2=3,w2=3,1);
+  Cnot.set(q1=3,w1=1,q2=3,w2=2,1);
+  Cnot.set(q1=3,w1=2,q2=3,w2=1,1);
+  Cnot.set(q1=3,w1=3,q2=3,w2=0,1);
+  
+  auto O=D1*Cnot;
+  O=D2*O;
+  
+  PrintData(O);
+  
+}
