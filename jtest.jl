9c2ce0f5c0f893d35aec832110ccc5a9b05012ca
smeeton <jeremy.p.rodriguez@mail.mcgill.ca>
2020-01-27 13:31:48 +0000

    object array making julia

--- jtest.jl
+++ jtest.jl
@@ -1,8 +1,12 @@
 x=1
 y=x
 for i=1:5
-  x=x+1
+  global x=x+1
 end
   
 println(x)
 println(y)
+
+z=[[1,2],[2,2],[3,3]]
+println(z)
+
---
+++ testread.jl
@@ -0,0 +1,58 @@
+using ITensors
+
+function check_str(a)
+    try
+        parse(Int64,a)
+        true
+    catch
+        false
+    end
+end
+ 
+ 
+ 
+ 
+ a=[]
+ open("testinput") do f
+   i=1
+   while !eof(f)
+    x=readline(f)
+    append!(a,[split(x)])
+#    println(x)
+   end
+ end
+if check_str(a[1][1])
+ # println(parse(Int,a[1][1]))
+end
+# println(a[3][3])
+println(a)
+b=[]
+for i =1:size(a,1)
+    c=[]
+    for j =1:size(a[i],1)
+       
+       if check_str(a[i][j])
+          append!(b,parse(Int64,a[i][j]))
+       else
+          append!(b,a[i][j])
+       end
+    end
+    #append!(b,c)
+end
+if check_str(a[1][1])
+    d=Index[]
+    for i=1:parse(Int64,a[1][1])
+        append!(d,[Index(4),Index(4)])
+    end
+end
+
+l=Index(4,"l")
+# reshape(b,(3,2))
+# 
+ println(d)
+ println(d[3])
+ d=reshape(d,(parse(Int64,a[1][1]),2))
+ println(d)
+ println(d[2,1])
+# println(size(b,1))
+# println(size(a,1))
