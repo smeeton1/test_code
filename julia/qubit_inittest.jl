363bf2b9df27210f512bfb820771611d8de0a666
smeeton <jeremy.p.rodriguez@mail.mcgill.ca>
2020-08-05 14:09:10 +0100

    small working tensor sim

--- julia/qubit_inittest.jl
+++ julia/qubit_inittest.jl
@@ -1,7 +1,10 @@
-using ITensors      
+using ITensors     
+
+
+
 
 function HGate(i::Index, j::Index)
-  A = ITensor(i,j)
+  A = ITensor(ComplexF64,i,j)
 
   A[i(1),j(1)]=1.0/2.0
   A[i(1),j(2)]=1.0/2.0
@@ -25,7 +28,7 @@ function HGate(i::Index, j::Index)
 end 
 
 function XGate(i::Index, j::Index)
-  A = ITensor(i,j)
+  A = ITensor(ComplexF64,i,j)
 
   A[i(1),j(4)]=1
   A[i(2),j(3)]=1
@@ -37,7 +40,7 @@ function XGate(i::Index, j::Index)
 end 
 
 function CNotGate(i::Index, j::Index, k::Index, l::Index)
-  A = ITensor(i,j,k,l)
+  A = ITensor(ComplexF64,i,j,k,l)
 
   A[i(1),j(1),k(1),l(1)]=1
   A[i(1),j(2),k(1),l(2)]=1
@@ -62,7 +65,7 @@ end
 
 
 function Init_st(i::Index,a)
-  A = ITensor(i)
+  A = ITensor(ComplexF64,i)
 
   if isa(a,Array)
   A[i(1)]=parse(ComplexF64,a[1])
@@ -118,39 +121,20 @@ function Tensor_Setup(N,d,a)
  Ham=[]
  for i=N+2:size(a,1)
    if a[i][1] == "H"
-     if !isempty(Ham)
-       if hasinds(Ham[end],d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2])&&(order(Ham[end])==2)
-         Ham[end]=Ham[end]+HGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2])
-       else
-         push!(Ham,HGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2]) )
-       end
-     else
+     
        push!(Ham,HGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2]) )
-     end
+     
    end
    
    if a[i][1] == "X"
-     if !isempty(Ham)
-       if hasinds(Ham[end],d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2])&&(order(Ham[end])==2)
-         Ham[end]=Ham[end]+XGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2])
-       else
-         push!(Ham,XGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2]) )
-       end
-     else
+
        push!(Ham,XGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2]) )
-     end
+     
    end
    
    if a[i][1] == "CN"
-     if !isempty(Ham)
-       if hasinds(Ham[end],d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2],d[parse(Int64,a[i][3]),1],d[parse(Int64,a[i][3]),2])&&(order(Ham[end])==4)
-         Ham[end]=Ham[end]+CNotGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2],d[parse(Int64,a[i][3]),1],d[parse(Int64,a[i][3]),2])
-       else
-         push!(Ham,CNotGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2],d[parse(Int64,a[i][3]),1],d[parse(Int64,a[i][3]),2]) )
-       end
-     else
+    
        push!(Ham,CNotGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2],d[parse(Int64,a[i][3]),1],d[parse(Int64,a[i][3]),2]) )
-     end
    end
  end
 
@@ -160,23 +144,28 @@ end
 function Ten_Add(Tens)
  fin=[]
  N = size(Tens,1)
- #println(N)
+ println("Add N=",N)
  re=[]
  #append!(fin,Tens[1])
  append!(re,0)
+ k=0
  for i=1:N
   # println(Tens[i])
    if !(i in re)
    push!(fin,Tens[i])
+
+   k=k+1
+   index=inds(fin[k])
    ad = true
    j=i+1
    while ad
-    #println(j)
+    println("j=",j)
     if j>N
       ad = false
-    elseif hasinds(Tens[j],Tens[i])
-     if order(Tens[i])==order(Tens[j])
-        fin[i]=fin[i]+Tens[j]
+      
+    elseif hasinds(Tens[j],index)
+     if order(fin[k])==order(Tens[j])
+        fin[k]=fin[k]+Tens[j]
         append!(re,j)
         j=j+1
      else
@@ -195,7 +184,62 @@ function Ten_Add(Tens)
  
 end
 
+function Ten_split(T)
+ fin=[]
+ N = size(T,1)
+ for i=1:N
+  if order(T[i])>2
+   index=inds(T[i])
+   U,S,V =svd(T[i],(index[1],index[2])) 
+   U=U*S
+   push!(fin,U)
+   while order(V)>3
+    index=inds(V)
+    n=size(index,1)
+    U,S,V =svd(V,(index[1],index[2],index[n]))
+    U=U*S
+    push!(fin,U)
+   end
+   push!(fin,V)
+  
+  else
+   push!(fin,T[i])
+  end
+ 
+ end
 
+ return fin
+ 
+end
+
+function line_mps(Q,T)
+ N=size(T,1)
+ #println("N=" ,N)
+ index=inds(Q)
+ #println(index)
+ for i=1:N
+   #println(hasinds(T[i],index))
+   if hasinds(T[i],index)
+     Q=Q*T[i]
+     #println(inds(T[i]))
+  
+   end
+  
+ end
+
+ return Q
+
+end
+
+function Mtra(i::Index)
+  A = ITensor(ComplexF64,i)
+
+  A[i(1)]=1
+  A[i(4)]=1
+
+  return A
+
+end 
 
 
  a=[]
@@ -215,25 +259,59 @@ d=Index_setup(parse(Int64,a[1][1]))
 println('2')
 Q=Inital_State(parse(Int64,a[1][1]),d,a)
 
-#println(Q)
+println(Q)
 # println(HGate(d[1,1],d[1,2]))
 println('3')
 Ham = Tensor_Setup(parse(Int64,a[1][1]),d,a)
 println('4')
 # println(size(Ham,1))
 # println(Ham[4])
-Ham = Ten_Add(Ham)
+#Ham = Ten_Add(Ham)
 println('5')
-T= CNotGate(d[1,1],d[1,2],d[2,1],d[2,2])
-println(T)
-println("svd of T")
-U,S,V =svd(T,(d[1,1],d[1,2]))
-println("U=",U)
-println("S=",S)
-println("V=",V)
+Ham = Ten_split(Ham)
+println('6')
+
+
+
+Tr=ITensor[]
+push!(Tr,Mtra(d[1,1]))
+push!(Tr,Mtra(d[2,2]))
+push!(Tr,Mtra(d[3,2]))
+
+Q[1]= line_mps(Q[1],Ham)
+Q[2]= line_mps(Q[2],Ham)
+Q[3]= line_mps(Q[3],Ham)
+println('7')
+T1=Q[3]*Q[2]*Q[1]*Tr[1]*Tr[2]
+T2=Q[2]*Q[1]*Q[3]*Tr[2]*Tr[3]
+T3=Q[2]*Q[1]*Q[3]*Tr[1]*Tr[3]
+
+# if T1==T2
+#  println("T")
+# else
+#  println("F")
+# end
+println(T1)
+println(T2)
+println(T3)
+
+# println(Q[1])
+# println(Q[2])
+# println(Q[3])
+println(norm(T1),norm(T2),norm(T3))
 #println(hasinds(Ham[1],Ham[2]))
-println(size(Ham,1))
-println(Ham)
+#println("Ham size =" ,size(Ham,1))
+#println(Ham)
  
  
- 
+#     if a[i][1] == "Y"
+#      if !isempty(Ham)
+#        if hasinds(Ham[end],d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2])&&(order(Ham[end])==2)
+#          Ham[end]=Ham[end]+YGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2])
+#        else
+#          push!(Ham,YGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2]) )
+#        end
+#      else
+#        push!(Ham,YGate(d[parse(Int64,a[i][2]),1],d[parse(Int64,a[i][2]),2]) )
+#      end
+#    end
